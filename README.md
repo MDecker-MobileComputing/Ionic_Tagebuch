28a4a9a3ba813d4d777e623c8a4137f998b6740a
MDecker-MobileComputer <mide_42b@yahoo.de>
2021-04-22 21:50:28 +0200

    Status Projekt vor Einbau Plugin.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ionic app to demonstrate usage of a Capacitor plugin #
+
+The repository contains an Ionic app for Android which is a very simple diary app.
+All texts entered are written into a text file on the filesystem of the Android device,
+for which the Capacitor plugin [filesystem](https://capacitorjs.com/docs/apis/filesystem) is used.
+
+<br>
+
+----
+
+## License ##
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License) for the files in this repository.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,68 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  /** Inhalt von Textarea mit Tagebucheintrag. */
+  private eintrag = "";
+
+
+ /**
+   * Konstruktor mit leerem Rumpf wird benötigt, um AlertController-
+   * und ToastController-Instanz als Member-Variable zu erhalten.
+   */
+  constructor( private alertCtrl      : AlertController,
+               private toastController: ToastController ) {}
+
+
+  /**
+   * Event-Handler für Button "Eintrag speichern".
+   */
+  private onEintragSpeichern() {
+
+    const eintragTrimmed = this.eintrag.trim();
+
+    if (eintragTrimmed.length === 0) {
+
+      this.zeigeDialog("Fehler", "Kein Text eingegeben.");
+      return;
+    }
+
+    this.eintrag = "";
+  }
+
+
+  /**
+   * Alert anzeigen, siehe auch https://ionicframework.com/docs/api/alert
+   *
+   * @param titel Titel des Dialogs
+   * @param nachricht Im Dialog anzuzeigender Text
+   */
+   async zeigeDialog(titel: string, nachricht: string) {
+
+    const meinAlert =
+          await this.alertCtrl.create({
+              header  : titel,
+              message : nachricht,
+              buttons : [ "Ok" ]
+          });
+
+    await meinAlert.present();
+  }
+
+
+  /**
+   * Toast anzeigen, siehe auch https://ionicframework.com/docs/api/toast
+   *
+   * @param nachricht Im Toast anzuzeigender Text
+   */
+  async zeigeToast(nachricht: string) {
+
+    const toast =
+          await this.toastController.create({
+              message : nachricht,
+              duration: 2000  // 2000 ms = 2 seconds
+          });
+
+    await toast.present();
+  }
 
 }
